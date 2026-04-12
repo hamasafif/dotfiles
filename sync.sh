7fcee95d71fdea179c0457b5468a5cf19da4c8d8
wrjunior <cyberhamas@gmail.com>
2026-04-12 12:13:37 +0700

    Tambahan File Sync

---
+++ sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "🔄 Memulai sinkronisasi dari sistem ke folder dotfiles..."
+
+# Pastikan folder tujuan ada
+mkdir -p .config/{bspwm,sxhkd,polybar,rofi,alacritty,picom}
+
+# Menyalin konfigurasi dari ~/.config
+echo "📂 Menyalin folder .config..."
+cp -r ~/.config/bspwm/* .config/bspwm/
+cp -r ~/.config/sxhkd/* .config/sxhkd/
+cp -r ~/.config/polybar/* .config/polybar/
+cp -r ~/.config/rofi/* .config/rofi/
+cp -r ~/.config/alacritty/* .config/alacritty/
+cp -r ~/.config/picom/* .config/picom/
+
+# Menyalin konfigurasi Zsh di home directory
+echo "🐚 Menyalin .zshrc..."
+cp ~/.zshrc ./
+
+echo "✅ Sinkronisasi selesai! File di ~/dotfiles sudah paling up-to-date."
